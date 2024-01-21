673e52633dad88b678e3c947923db18ad6f31ed6
Dzmitry Yarmoshkin <spanb4@gmail.com>
2024-01-21 21:36:05 +0300

    add modules subtask

---
+++ src/modules/esm.mjs
@@ -0,0 +1,42 @@
+import path from 'node:path';
+import { release, version } from 'node:os';
+import { createServer as createServerHttp } from 'node:http';
+import * as jsonA from './files/a.json' assert { type: 'json' };
+import * as jsonB from './files/b.json' assert { type: 'json' };
+import { fileURLToPath } from 'node:url';
+import('./files/c.mjs');
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const random = Math.random();
+
+let unknownObject;
+
+if (random > 0.5) {
+  unknownObject = jsonA;
+} else {
+  unknownObject = jsonB;
+}
+
+console.log(`Release ${release()}`);
+console.log(`Version ${version()}`);
+console.log(`Path segment separator is "${path.sep}"`);
+
+console.log(`Path to current file is ${__filename}`);
+console.log(`Path to current directory is ${__dirname}`);
+
+const myServer = createServerHttp((_, res) => {
+  res.end('Request accepted');
+});
+
+const PORT = 3000;
+
+console.log(unknownObject);
+
+myServer.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+  console.log('To terminate it, use Ctrl+C combination');
+});
+
+export { unknownObject, myServer };
