1a4669382e4c8922e901bc219160406916d42d10
Dzmitry Yarmoshkin <spanb4@gmail.com>
2024-01-21 21:15:08 +0300

    add cli subtask

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,14 @@
 const parseArgs = () => {
-    // Write your code here 
+  const resArr = [];
+  const args = process.argv.slice(2);
+
+  for (let i = 0; i < args.length; i += 2) {
+    if (args[i].startsWith('--')) {
+      resArr.push(`${args[i].slice(2)} is ${args[i + 1]}`);
+    }
+  }
+
+  console.log(resArr.join(', '));
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,13 @@
 const parseEnv = () => {
-    // Write your code here 
+  let resArr = [];
+
+  for (let key in process.env) {
+    if (key.startsWith('RSS_')) {
+      resArr.push(`${key}=${process.env[key]}`);
+    }
+  }
+
+  console.log(resArr.join('; '));
 };
 
 parseEnv();
