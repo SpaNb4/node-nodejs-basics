dbe244bbb7c4558f3abd80de59d48089c34189dc
Dzmitry Yarmoshkin <spanb4@gmail.com>
2024-01-21 21:45:57 +0300

    add hash subtask

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,25 @@
+import { createHash } from 'node:crypto';
+import { createReadStream } from 'node:fs';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const sourceFilePath = path.join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+
 const calculateHash = async () => {
-    // Write your code here 
+  const hash = createHash('sha256');
+
+  const input = createReadStream(sourceFilePath);
+  input.on('readable', () => {
+    const data = input.read();
+    if (data) {
+      hash.update(data);
+    } else {
+      console.log(`${hash.digest('hex')}`);
+    }
+  });
 };
 
 await calculateHash();
