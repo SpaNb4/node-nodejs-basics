754f71351bb10b54ded10288814b5c26bba49445
Dzmitry Yarmoshkin <spanb4@gmail.com>
2024-01-25 21:48:03 +0300

    add child processes subtask

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,17 @@
+import { fork } from 'node:child_process';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const childFilePath = path.join(__dirname, 'files', 'script.js');
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  fork(childFilePath, args);
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+const someArgument1 = 'qwerty';
+const someArgument2 = 222;
+
+spawnChildProcess([someArgument1, someArgument2]);
